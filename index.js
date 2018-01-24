cf9d0c80d3e39833ad9fcee7897ee7ef7148a8be
Bo Eun Lee <bolee417@gmail.com>
2018-01-24 08:55:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,21 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * (100 - 1) + 1);
+  var itemObject = {};
+  itemObject[item] = price;
+  cart.push(itemObject);
+ console.log(`${item} has been added to your cart.`);
+ return cart; 
 }
+
 
 function viewCart() {
   // write your code here
+  if (cart[item].length === 0) {
+    return "Your shopping cart is empty."
+  }
+  else if (cart[item].length ===1) {
+  for (var i = 0; i < cart.length; i++) {
+    for (var n = 0; n <cart[i].length; n++){
+    return `In your cart, you have ${cart[i]} at $${cart[i][n]}.`; 
+  }
+}
+}
 }
 
 function total() {
-  // write your code here
+
 }
 
 function removeFromCart(item) {
   // write your code here
+
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
